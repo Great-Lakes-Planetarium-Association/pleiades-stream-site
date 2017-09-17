1448a15baea711e90c2fd2c908b0a543848618c2
Brandon Raitt <administrator@pxoink.net>
2017-09-17 19:30:55 -0400

    Update the overall theme by adjusting the base design toward a darker color scheme.

--- public/index.php
+++ public/index.php
@@ -2,24 +2,37 @@
 <html lang="en">
 	<head>
 		<title></title>
+		<meta charset="utf-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=Edge" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<link rel="author" content="GLPA" />
 		<link rel="group" href="humans.txt" />
 		<link rel="stylesheet" href="css/stylesheet.css" />
 	</head>
 	<body>
 		<header>
+			<div class="row">
+				<div class="column small-12 medium-3 large-2">
+					<img src="images/logo.png" alt="GLPA logo" />
+				</div>
+				<div class="column show-for-medium medium-7 large-9">
+					<h1>Live From the GLPA Conference</h1>
+				</div>
+				<div class="column show-for-medium medium-2 large-1">
 
+				</div>
+			</div>
 		</header>
 		<section>
-			<section>
-				<main>
+			<section class="row">
+				<main class="column small-12 medium-9 large-10">
+					<div class="responsive-embed">
 
+					</div>
 				</main>
-				<aside>
+				<aside class="column show-for-medium medium-3 large-2">
 
 				</aside>
-			</section>
-			<section>
-
 			</section>
 		</section>
 		<footer>
