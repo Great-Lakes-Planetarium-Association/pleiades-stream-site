23fa694b7f109853a385a3cd3750e2f799fb3d94
Brandon Raitt <administrator@pxoink.net>
2017-09-17 20:07:39 -0400

    Update the layout to begin organizing content.

--- public/index.php
+++ public/index.php
@@ -12,25 +12,58 @@
 	<body>
 		<header>
 			<div class="row">
-				<div class="column small-12 medium-3 large-2">
+				<div class="column small-12 medium-2 large-1">
 					<img src="images/logo.png" alt="GLPA logo" />
 				</div>
 				<div class="column show-for-medium medium-7 large-9">
 					<h1>Live From the GLPA Conference</h1>
 				</div>
-				<div class="column show-for-medium medium-2 large-1">
-
+				<div class="column show-for-medium medium-3 large-2 text-right">
+					<a href="https://twitter.com/Pleiades_NPC" class="sobar" target="_blank" rel="noopener">
+						<span class="social micro twitter" title="Twitter">Twitter</span>
+					</a>
 				</div>
 			</div>
 		</header>
 		<section>
 			<section class="row">
-				<main class="column small-12 medium-9 large-10">
-					<div class="responsive-embed">
+				<main class="column small-12 medium-9">
+					<div class="callout alert">
+						Our broadcast has not yet begun.
+						We'll begin transmitting for paper sessions in the morning on Thursday, October 20.
+					</div>
+					<nav>
+						<ul class="menu">
+							<li>
+								<h2>Choose a Stream:</h2>
+							</li>
+							<li>
+								<a href="#video" class="button" data-stream-type="">
+									Video 1
+								</a>
+							</li>
+							<li>
+								<a href="#video" class="button" data-stream-type="">
+									Video 2
+								</a>
+							</li>
+							<li>
+								<a href="#video" class="button" data-stream-type="">
+									Video 3
+								</a>
+							</li>
+							<li>
+								<a href="#video" class="button" data-stream-type="">
+									Audio
+								</a>
+							</li>
+						</ul>
+					</nav>
+					<div id="video" class="responsive-embed">
 
 					</div>
 				</main>
-				<aside class="column show-for-medium medium-3 large-2">
+				<aside class="column show-for-medium medium-3">
 
 				</aside>
 			</section>
